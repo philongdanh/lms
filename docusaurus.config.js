9cded5e05352ddf3fbbc9ef16e25792e01bc9350
Danh Phi Long <120664940+idlongggg@users.noreply.github.com>
2026-01-21 16:09:22 +0700

    docs: update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,6 +38,7 @@ async function createConfig() {
                   ext: 'svg',
                   defaultD2Opts: [
                     '--layout=elk',
+                    '--sketch',
                     '--font-regular=./static/fonts/google-sans-code/GoogleSansCode-Regular.ttf',
                     '--font-bold=./static/fonts/google-sans-code/GoogleSansCode-Bold.ttf',
                     '--font-italic=./static/fonts/google-sans-code/GoogleSansCode-Italic.ttf',
